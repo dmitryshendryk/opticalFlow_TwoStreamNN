9ef5bea6e56488b5b06b9dd9186d8eb81412f012
Dmitry <zaonmax@inbox.ru>
2019-04-10 22:30:28 +0900

    split opt_flow

--- opt_flow/main.cpp
+++ opt_flow/main.cpp
@@ -1,11 +1,41 @@
 #include<iostream>
+#include "opt_flow.hpp"
 using namespace std;
-int main()
+int main(int argc, char *argv[] )
 {
-    int a=6, b=4;
-    cout<<a+b<<endl;
-    cout<<"test passed"<<endl;
-    cin.get();
-    cin.get();
+
+    const char* keys = 
+				"{ help h usage ? |       | print this message   }"
+				"{ start_video    |   1   | start video id       }"
+				"{ gpuID          |   1   | use this gpu         }"
+				"{ type           |   1   | use this flow method (0=Brox, 1=TV-L1)}"
+				"{ skip           |   1   | frame skip           }"
+				"{ vid_path       |  ./   | path input (where the videos are)}"
+				"{ out_path       |  ./   | path output          }";
+
+    CommandLineParser parser(argc, argv, keys);
+
+	if (parser.get<bool>("help"))
+	{
+		cout << "Usage: compute_flow [options]" << endl;
+		cout << "Avaible options:" << endl;
+		parser.printMessage();
+		return 0;
+	}
+
+	if (argc > 1) {
+		start_with_vid = parser.get<int>("start_video");
+		gpuID = parser.get<int>("gpuID");
+		type = parser.get<int>("type");
+		frameSkip = parser.get<int>("skip");
+		vid_path = parser.get<std::string>("vid_path");
+		out_path = parser.get<std::string>("out_path");
+		out_path_jpeg = out_path + "/rgb/";
+		cout << "start_vid:" << start_with_vid << "gpuID:" << gpuID << "flow method: "<< type << " frameSkip: " << frameSkip << " vid_path: " << vid_path << " out_path" << out_path << " jpegs: " << out_path_jpeg << endl;
+	}
+
+    pre_process::OpticalFlow opt_flow;
+    opt_flow.compute_Flow(int start_with_vid, int gpuID, int type, int frameSkip,
+                        String vid_path, String out_path, String out_path_jpeg);
     return 0;
 }
---
+++ opt_flow/opt_flow.hpp
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <sys/time.h>
+#include <time.h>
+#include <sstream>
+
+#include <QDirIterator>
+#include <QFileInfo>
+#include <QString>
+
+#include <opencv2/core/core.hpp>
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/cuda.hpp"
+#include "opencv2/cudaoptflow.hpp"
+
+#include <dirent.h>
+
+
+using namespace std;
+using namespace cv;
+using namespace cv::cuda;
+
+namespace pre_process 
+{
+
+    float MIN_SZ = 256;
+    float OUT_SZ = 256;
+
+    bool clipFlow = true; // clips flow to [-20 20]
+    bool resize_img = false;
+
+    // These are default paths
+
+    std::string vid_path = "/home/katou2/github-home/gpu_flow/build/test";
+    std::string out_path	= "/home/katou2/github-home/gpu_flow/build/test_out";
+    std::string out_path_jpeg	= "/home/katou2/github-home/gpu_flow/build/test_out";
+
+    bool createOutDirs = true;
+
+    /* THESE ARE MY PARAMS, NOT FEICHENHOFER'S */
+
+    bool debug = false;
+    bool rgb = false;
+    bool bins = false;
+
+    // Global variables for cuda::BroxOpticalFlow
+    const float alpha_ = 0.197;
+    const float gamma_ = 50;
+    const float scale_factor_ = 0.8;
+    const int inner_iterations_ = 10;
+    const int outer_iterations_ = 77;
+    const int solver_iterations_ = 10;
+
+    const int RESIZE_WIDTH = 224;
+    const int RESIZE_HEIGHT = 224;
+    const bool warp = false;
+
+    class OpticalFlow 
+    {
+
+        GpuMat frame0GPU, frame1GPU, flowGPU;
+        Mat frame0_rgb_, frame1_rgb_, frame0_rgb, frame1_rgb, frame0, frame1, rgb_out;
+        Mat frame0_32, frame1_32, imgU, imgV;
+        Mat motion_flow, flow_rgb;
+        Mat flowCPU, planes[3], mag;
+        char cad[N_CHAR];
+        struct timeval tod1;
+        double t1 = 0.0, t2 = 0.0, tdflow = 0.0, t1fr = 0.0, t2fr = 0.0, tdframe = 0.0;
+
+        int start_with_vid = 1;
+        int gpuID = 0;
+        int type = 1;
+        int frameSkip = 1;
+
+        OpticalFlow();
+        ~OpticalFlow();
+        void compute_Flow(int start_with_vid, int gpuID, int type, int frameSkip,
+                        String vid_path, String out_path, String out_path_jpeg);
+    };
+}
