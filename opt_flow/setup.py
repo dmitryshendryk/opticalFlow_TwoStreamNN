1048be987c35cc503e582cf63ded2caf7d308fb3
Dmitry <zaonmax@inbox.ru>
2019-04-11 22:47:36 +0900

    hotfix

--- opt_flow/setup.py
+++ opt_flow/setup.py
@@ -12,10 +12,13 @@ incDirs = ['/usr/local/include',
 
 libDirs = ['/usr/local/lib', '/home/dmitry/Qt/5.9/gcc_64/lib']
 
+cflags = ['-std=c++11']
+
 setup(ext_modules=[Extension("optical_flow_wrapper", 
                              ["optical_flow_wrapper.pyx", 
                               "opt_flow.cpp"], 
                               include_dirs=incDirs,
                               library_dirs=libDirs,
-                              language="c++",)],
+                              language="c++",
+                              extra_compile_args=cflags)],
       cmdclass = {'build_ext': build_ext})
