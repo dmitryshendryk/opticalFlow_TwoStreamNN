73b755527a96258fe3f4709f736ed3c392cf3703
Dmitry <zaonmax@inbox.ru>
2019-04-11 20:54:53 +0900

    hotfix

--- opt_flow/setup.py
+++ opt_flow/setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
@@ -6,5 +6,6 @@ setup(ext_modules=[Extension("optical_flow_wrapper",
                              ["optical_flow_wrapper.pyx", 
                               "opt_flow.cpp"], 
                               libraries=['Qt5Widgets', 'OpenCV'],
+                              packages = find_packages(),
                               language="c++",)],
       cmdclass = {'build_ext': build_ext})
