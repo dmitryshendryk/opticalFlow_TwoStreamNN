2af5f3fa51ce2816188dd4c2f7b28028b748097d
Dmitry <zaonmax@inbox.ru>
2019-04-11 21:05:44 +0900

    hotfix

--- opt_flow/setup.py
+++ opt_flow/setup.py
@@ -1,11 +1,19 @@
-from distutils.core import setup, find_packages
+from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
+import numpy as np
+
+incDirs = ['/usr/local/include',
+           '/usr/local/cuda/include',
+           np.get_include()]
+
+libDirs = ['/usr/local/lib']
 
 setup(ext_modules=[Extension("optical_flow_wrapper", 
                              ["optical_flow_wrapper.pyx", 
                               "opt_flow.cpp"], 
-                              libraries=['Qt5Widgets', 'OpenCV'],
-                              packages = find_packages(),
+                              include_dirs=incDirs,
+                              library_dirs=libDirs,
                               language="c++",)],
       cmdclass = {'build_ext': build_ext})
