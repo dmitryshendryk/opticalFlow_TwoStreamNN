72337f6d0a52ffb9d9bf6d00f94a823dd40086ce
Dmitry <zaonmax@inbox.ru>
2019-04-12 22:04:10 +0900

    hotfix

--- opt_flow/python/optical_flow_wrapper.pyx
+++ opt_flow/python/optical_flow_wrapper.pyx
@@ -1,23 +1,14 @@
 
 from libcpp.string cimport string
+from libcpp.double cimport double 
 
 cdef extern from "../opt_flow.hpp" namespace "pre_process":
     cdef cppclass OpticalFlow:
         OpticalFlow();
-        
+
         int compute_Flow(int start_with_vid, int gpuID, int type, int frameSkip,
                         string vid_path, string out_path, string out_path_jpeg);
-    cdef cppclass GpuMat:
-        GpuMat frame0GPU, frame1GPU, flowGPU;
-    
-    cdef cppclass Mat:
-        Mat frame0_rgb_, frame1_rgb_, frame0_rgb, frame1_rgb, frame0, frame1, rgb_out;
-        Mat frame0_32, frame1_32, imgU, imgV;
-        Mat motion_flow, flow_rgb;
-        Mat flowCPU, planes[3], mag;
-    
-    char cad[500];
-    double t1 = 0.0, t2 = 0.0, tdflow = 0.0, t1fr = 0.0, t2fr = 0.0, tdframe = 0.0;
+
 
     
 
